30e0a917884f0275bd5a1aa8e0985c644441c580
swblaster <swblaster84@gmail.com>
2020-05-08 16:32:01 -0500

    Test model is implemented.

--- train.py
+++ train.py
@@ -7,10 +7,11 @@ Northwestern University
 import tensorflow as tf
 import argparse
 from feeder import cosmoflow
+from model import model
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--batch_size", type = int, default = 8,
+    parser.add_argument("-b", "--batch_size", type = int, default = 4,
                         help = "number of training samples for each mini-batch")
     parser.add_argument("-e", "--epochs", type = int, default = 1,
                         help = "number of epochs")
@@ -24,4 +25,7 @@ if __name__ == "__main__":
     ds = cosmoflow("test.yaml", batch_size = args.batch_size)
     ds.shuffle()
     train_ds = ds.train_dataset()
-    images, labels = train_ds.next()
+    cosmo_model = model()
+    mymodel = cosmo_model.build_model()
+    for i in range (10):
+        images, labels = train_ds.next()
