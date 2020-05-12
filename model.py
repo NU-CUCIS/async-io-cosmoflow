ccfa3e89a01d08a09f27521d7878d42d0618c673
swblaster <swblaster84@gmail.com>
2020-05-12 16:29:35 -0500

    Implemented model.py

---
+++ model.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv3D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import AveragePooling3D
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Flatten
+
+class model ():
+    def __init__ (self, num_filters = 16, kernel_size = 3, pool_size = 2):
+        self.num_filters = num_filters
+        self.kernel_size = kernel_size
+        self.pool_size = pool_size
+        print ("Initializing cosmoflow model...")
+
+    def build_model(self):
+        x_in = Input(shape = (128, 128, 128, 12), name = "input")
+        h = Conv3D(16, self.kernel_size, activation = 'linear', name = 'conv1')(x_in)
+        h = LeakyReLU(name = 'relu1')(h)
+        h = AveragePooling3D(pool_size = self.pool_size, name = 'pool1')(h)
+
+        h = Conv3D(32, self.kernel_size, activation = 'linear', name = 'conv2')(h)
+        h = LeakyReLU(name = 'relu2')(h)
+        h = AveragePooling3D(pool_size = self.pool_size, name = 'pool2')(h)
+
+        h = Conv3D(64, self.kernel_size, activation = 'linear', name = 'conv3')(h)
+        h = LeakyReLU(name = 'relu3')(h)
+        h = AveragePooling3D(pool_size = self.pool_size, name = 'pool3')(h)
+
+        h = Conv3D(128, self.kernel_size, activation = 'linear', name = 'conv4')(h)
+        h = LeakyReLU(name = 'relu4')(h)
+        h = AveragePooling3D(pool_size = self.pool_size, name = 'pool4')(h)
+
+        h = Conv3D(256, self.kernel_size, activation = 'linear', name = 'conv5')(h)
+        h = LeakyReLU(name = 'relu5')(h)
+        h = AveragePooling3D(pool_size = self.pool_size, name = 'pool5')(h)
+
+        h = Conv3D(256, self.kernel_size, activation = 'linear', name = 'conv5')(h)
+        h = LeakyReLU(name = 'relu6')(h)
+
+        h = Conv3D(256, self.kernel_size, activation = 'linear', name = 'conv5')(h)
+        h = LeakyReLU(name = 'relu7')(h)
+
+        h = Flatten(name = 'pool_to_full')(h)
+        h = Dense(2048, activation = 'linear', name = 'full1')(h)
+        h = LeakyReLU(name = 'relu8')(h)
+
+        h = Dense(256, activation = 'linear', name = 'full2')(h)
+        h = LeakyReLU(name = 'relu9')(h)
+
+        h = Dense(4, activation = 'linear', name = 'full3')(h)
+        y = LeakyReLU(name = 'relu10')(h)
+        return Model(inputs = x_in, outputs = y)
