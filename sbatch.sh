e81c689823fb0fa7eff16694a3f2da5ae08295dc
Sunwoo Lee <swblaster84@gmail.com>
2020-07-29 09:08:46 -0700

    Updated the default sbatch setting to 64 GPUs training.

--- sbatch.sh
+++ sbatch.sh
@@ -2,15 +2,15 @@
 
 #SBATCH -t 00:10:00
 #SBATCH --qos=regular
-#SBATCH --nodes=2
+#SBATCH --nodes=8
 #SBATCH --constraint=gpu
-#SBATCH -G 16
+#SBATCH -G 64
 #SBATCH --exclusive
 #SBATCH -A m844
 
 ulimit -c unlimited
 
-srun -n 16 -c 10 python3 main.py --epochs=3 \
+srun -n 64 -c 10 python3 main.py --epochs=3 \
                                  --batch_size=4 \
                                  --overlap=1 \
                                  --checkpoint=0 \
